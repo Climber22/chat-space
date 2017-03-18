e43421a79032138cd2ec5976125129d59b200148
Climber22 <ihaya.shohei@me.com>
2017-03-19 00:06:20 +0900

    remove mailer setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #mailer setting
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
