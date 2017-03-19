21b72fefcc5b755e98fd438f5f7a7a8039597e48
Climber22 <ihaya.shohei@me.com>
2017-03-20 02:18:09 +0900

    fix group_users table

--- db/migrate/20170319171107_create_group_users.rb
+++ db/migrate/20170319171107_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
-      t.references :user_id
-      t.references :group_id
+      t.references :user
+      t.references :group
       t.timestamps
     end
   end
