5384e366e5fd880ea5887ea4c8594d07783c8fc0
Climber22 <ihaya.shohei@me.com>
2017-03-20 00:42:57 +0900

    remove adding strong parameter at sign_in delete trace of profy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    # sign_inのときに、group_keyも許可する
-      devise_parameter_sanitizer.permit(:sign_in, keys:[:name])
-    # sign_upのときに、group_keyも許可する
+    # sign_upのときに、nameも許可する
       devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-    #account_updateのときに、group_keyも許可する
+    #account_updateのときに、nameも許可する
       devise_parameter_sanitizer.permit(:account_update, keys:[:name])
   end
 end
