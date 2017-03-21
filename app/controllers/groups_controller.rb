dc25e9be94e0fd5a2ce4bbc9bf49e205a27e79f4
Climber22 <ihaya.shohei@me.com>
2017-03-22 03:32:21 +0900

    define some actions in group_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,5 +6,20 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:group_id])
+  end
+
+  def create
+    group = Group.create(group_params)
+    redirect_to controller: "messages", action: "index", group_id: group.id
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name,:user_ids=>[])
   end
 end
