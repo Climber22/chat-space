5abbe7b92b0b2d42dd4c63e924122cea96c7baf4
Climber22 <ihaya.shohei@me.com>
2017-03-22 04:56:15 +0900

    define long description in controller instead of partial template

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
+    @group_menbers = Group.find(params[:group_id]).users.pluck(:name).join(" ")
     @message = ""
   end
 
