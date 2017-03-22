b8897538561bc30c58c14417eaefc4bd078d8f56
Climber22 <ihaya.shohei@me.com>
2017-03-22 22:04:58 +0900

    remove unnecessary description

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,4 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-
-  # 以下の記述でgroupを保存した際に同時にuserも保存してくれるようになる
-  accepts_nested_attributes_for :users
 end
