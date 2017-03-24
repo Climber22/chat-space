0620650d24f626d5c1f9469743691a6e6185b6ea
Climber22 <ihaya.shohei@me.com>
2017-03-24 19:36:20 +0900

    omit then

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ class GroupsController < ApplicationController
 
   def create
     group = Group.new(group_params)
-    if group.save then
+    if group.save
     redirect_to group_messages_path(group), notice: "Successfully create group."
     else
       render action: "new", notice: "Unfortunately, you failed to create group. Please try again."
@@ -23,7 +23,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    if @group.update(group_params) then
+    if @group.update(group_params)
     redirect_to group_messages_path(@group), notice: "successfully update group"
   else
     render action: "edit", notice: "Unfortunately, you failed to edit group. Please try again."
