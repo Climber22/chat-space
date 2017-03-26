d7506249b4f9002be1df0976357da3b5481661f5
Climber22 <ihaya.shohei@me.com>
2017-03-26 17:28:37 +0900

    make messages table

--- db/migrate/20170326082300_create_messages.rb
+++ db/migrate/20170326082300_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-
+      t.text :body, null: false
+      t.references :user_id, null: false
+      t.references :group_id, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322133816) do
+ActiveRecord::Schema.define(version: 20170326082300) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20170322133816) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "body",        limit: 65535, null: false
+    t.integer  "user_id_id",                null: false
+    t.integer  "group_id_id",               null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["group_id_id"], name: "index_messages_on_group_id_id", using: :btree
+    t.index ["user_id_id"], name: "index_messages_on_user_id_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
