d8c16a7dbb09bcbd90b0b3ed4da5a205ceab6511
Climber22 <ihaya.shohei@me.com>
2017-04-01 20:44:18 +0900

    add test about redirect without sign in

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,51 +4,75 @@ describe MessagesController do
   let(:user) {create(:user)}
   let(:group) {create(:group)}
   let(:message) {create(:message, group_id: group.id, user_id:user.id)}
-  before do
-    sign_in user
-  end
+
   describe "GET #index" do
-    before do
-      get :index, params: {group_id: group.id}
+    describe "when user signed in" do
+
+      before do
+        sign_in user
+        get :index, params: {group_id: group.id}
+      end
+
+      it "assigns the requested groups to @groups" do
+        groups = user.groups
+        expect(assigns(:groups)).to match(groups)
+      end
+      it "assigns the requested group to @group" do
+        expect(assigns(:group)).to match(group)
+      end
+      it "assigns the requested message to @message" do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+      it "assigns the requested messages to @messages" do
+        messages = group.messages
+        expect(assigns(:messages)).to match(messages)
+      end
+      it "renders the :index template" do
+        expect(response).to render_template :index
+      end
     end
 
-    it "assigns the requested groups to @groups" do
-      groups = user.groups
-      expect(assigns(:groups)).to match(groups)
-    end
-    it "assigns the requested group to @group" do
-      expect(assigns(:group)).to match(group)
-    end
-    it "assigns the requested message to @message" do
-      expect(assigns(:message)).to be_a_new(Message)
-    end
-    it "assigns the requested messages to @messages" do
-      messages = group.messages
-      expect(assigns(:messages)).to match(messages)
-    end
-    it "renders the :index template" do
-      expect(response).to render_template :index
+    describe "when user didn't sign in" do
+      it "redirects to user_session without sign in" do
+        get :index, params: {group_id: group.id}
+        expect(response).to redirect_to user_session_path
+      end
     end
   end
 
   describe "POST #create" do
-    describe "when body has content" do
-      it "renders the :index template if saving of message is success" do
-        message_c = attributes_for(:message, user_id: user.id, group_id: group.id)
-        post :create, params:{message: message_c, group_id: group.id}
-        expect(response).to redirect_to group_messages_path(group)
+    describe "when user signed in" do
+
+      before do
+        sign_in user
+      end
+
+      describe "when body has content" do
+        it "renders the :index template if saving of message is success" do
+          message_c = attributes_for(:message, user_id: user.id, group_id: group.id)
+          post :create, params:{message: message_c, group_id: group.id}
+          expect(response).to redirect_to group_messages_path(group)
+        end
+      end
+
+      describe "when body is nil" do
+        before do
+          message_f = attributes_for(:message, body: nil, user_id: user.id, group_id: group.id)
+          post :create, params:{message: message_f, group_id: group.id}
+        end
+        it "renders the :index template if saving of message is success" do
+          expect(response).to redirect_to group_messages_path(group)
+        end
+        it "makes error message when body of message is nil" do
+          expect(flash[:alert]).not_to be_empty
+        end
       end
     end
-    describe "when body is nil" do
-      before do
-        message_f = attributes_for(:message, body: nil, user_id: user.id, group_id: group.id)
-        post :create, params:{message: message_f, group_id: group.id}
-      end
-      it "renders the :index template if saving of message is success" do
-        expect(response).to redirect_to group_messages_path(group)
-      end
-      it "makes error message when body of message is nil" do
-        expect(flash[:alert]).not_to be_empty
+    describe "when user didn't sign in" do
+      it "redirects to user_session without sign in" do
+        message_c = attributes_for(:message, user_id: user.id, group_id: group.id)
+        post :create, params:{message: message_c, group_id: group.id}
+        expect(response).to redirect_to user_session_path
       end
     end
   end
