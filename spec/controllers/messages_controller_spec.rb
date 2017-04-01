f9f9c555fb47c16936848f38a655bfe8d80db8be
Climber22 <ihaya.shohei@me.com>
2017-04-01 20:11:45 +0900

    use new description style and omit message_c

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -34,17 +34,15 @@ describe MessagesController do
   describe "POST #create" do
     describe "when body has content" do
       it "renders the :index template if saving of message is success" do
-        message_c = (attributes_for (:message)).merge(user_id:user.id,group_id:group.id)
-        post :create, message: message_c, group_id: group.id
+        message_c = attributes_for(:message, user_id: user.id, group_id: group.id)
+        post :create, params:{message: message_c, group_id: group.id}
         expect(response).to redirect_to group_messages_path(group)
       end
     end
     describe "when body is nil" do
       before do
-        message_c = (attributes_for (:message)).merge(user_id:user.id,group_id:group.id)
-        message_f = message_c
-        message_f[:body] = nil
-        post :create, message: message_f, group_id: group.id
+        message_f = attributes_for(:message, body: nil, user_id: user.id, group_id: group.id)
+        post :create, params:{message: message_f, group_id: group.id}
       end
       it "renders the :index template if saving of message is success" do
         expect(response).to redirect_to group_messages_path(group)
