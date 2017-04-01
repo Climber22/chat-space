816d77d98d54c37bed3380e7a463b55bfe3426fc
Climber22 <ihaya.shohei@me.com>
2017-04-01 18:39:53 +0900

    define some factories

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,10 +1,9 @@
 FactoryGirl.define do
-
   factory :message do
     body "text"
-    user_id { rand(1..3) }
-    group_id { rand(1..3) }
     created_at { Faker::Time.between(7.days.ago, 4.days.ago, :all) }
     updated_at { Faker::Time.between(3.days.ago, Time.now, :all) }
+    user
+    group
   end
 end
