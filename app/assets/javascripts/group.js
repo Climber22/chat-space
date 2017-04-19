ed182ad4dd64c8a6b813be059c227d79370de1e8
Climber22 <ihaya.shohei@me.com>
2017-04-19 23:33:59 +0900

    fix camel case

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,30 +1,30 @@
 $(function(){
 
   function checkOverlap(user_id){
-    var result_lists = $(".user-search-result li");
-    var overlap_no = true;
-    $.each(result_lists, function(index, result_list){
-      var user_id_already = $(result_list).attr("data-user-id");
-      if( user_id_already == user_id){
-        overlap_no = false;
+    var resultLists = $(".user-search-result li");
+    var overlapNo = true;
+    $.each(resultLists, function(index, resultList){
+      var userIdAlready = $(resultList).attr("data-user-id");
+      if( userIdAlready == userId){
+        overlapNo = false;
       }
     });
 
-    var member_lists = $(".chat-group-member li");
-    $.each(member_lists, function(index, member_list){
-      var user_id_member = $(member_list).attr("data-user-id");
-      if( user_id_member == user_id ){
-        overlap_no = false;
+    var memberLists = $(".chat-group-member li");
+    $.each(memberLists, function(index, memberList){
+      var userIdMember = $(memberList).attr("data-user-id");
+      if( userIdMember == userId ){
+        overlapNo = false;
       }
     })
-    return overlap_no;
+    return overlapNo;
   }
 
   function buildHTMLNameResultNew(users){
     var html = $("<ul class = 'user-search-result'></ul>");
     $.each(users, function(index, user){
       if(checkOverlap(user.id)){
-        var name_area = `<li class = 'chat-group-user' data-user-id = ${user.id}>
+        var nameArea = `<li class = 'chat-group-user' data-user-id = ${user.id}>
                            <div class = "chat-group-user__name">
                              ${user.name}
                            </div>
@@ -34,7 +34,7 @@ $(function(){
                              </a>
                            </div>
                          </li>`;
-        html = $(html).append(name_area);
+        html = $(html).append(nameArea);
      }else{
       html = "";
      }
@@ -42,56 +42,56 @@ $(function(){
     return html;
   }
 
-  function buildHTMLNameResultAgain(html_result_again){
-    html_result_again.find("a").html("追加").removeClass("chat-group-user__btn--remove").addClass("chat-group-user__btn--add");
-    html_result_again.find("input").remove();
+  function buildHTMLNameResultAgain(htmlResultAgain){
+    htmlResultAgain.find("a").html("追加").removeClass("chat-group-user__btn--remove").addClass("chat-group-user__btn--add");
+    htmlResultAgain.find("input").remove();
     if( $("#chat-group-users ul").size() ){
-      html_result_again = $("<ul class = 'user-search-result'></ul>").append(html_result_again);
+      htmlResultAgain = $("<ul class = 'user-search-result'></ul>").append(htmlResultAgain);
     }
-    return html_result_again;
+    return htmlResultAgain;
   }
 
 
-  function buildHTMLNameStage(html_stage){
-    html_stage.find("a").html("削除").removeClass("chat-group-user__btn--add").addClass("chat-group-user__btn--remove");
-    var user_id = $(html_stage).attr("data-user-id");
-    var hidden_form = `<input name="group[user_ids][]" type="hidden" value = "${user_id}" />`;
-    $(html_stage).append(hidden_form);
-    return html_stage;
+  function buildHTMLNameStage(htmlStage){
+    htmlStage.find("a").html("削除").removeClass("chat-group-user__btn--add").addClass("chat-group-user__btn--remove");
+    var userId = $(htmlStage).attr("data-user-id");
+    var hiddenForm = `<input name="group[user_ids][]" type="hidden" value = "${userId}" />`;
+    $(htmlStage).append(hiddenForm);
+    return htmlStage;
   }
 
 
 
   $(document).on("click", ".chat-group-user__btn--add", function(e){
     e.preventDefault();
-    var html_result = $(this).parents("li");
+    var htmlResult = $(this).parents("li");
     $(this).parents("li").remove();
-    var html_stage = buildHTMLNameStage(html_result);
+    var htmlStage = buildHTMLNameStage(htmlResult);
     if( !($("#stage ul").size()) ){
       $("#stage").append("<ul class = 'chat-group-member'></ul>");
     }
-    $("#stage ul").append(html_stage);
+    $("#stage ul").append(htmlStage);
   });
 
   $(document).on("click", ".chat-group-user__btn--remove", function(e){
     e.preventDefault();
-    var html_result_again = $(this).parents("li");
+    var htmlResultAgain = $(this).parents("li");
     $(this).parents("li").remove();
-    html_result_again = buildHTMLNameResultAgain(html_result_again);
+    htmlResultAgain = buildHTMLNameResultAgain(htmlResultAgain);
     if( !($("#chat-group-users ul").size()) ){
       $("#chat-group-users").append("<ul class = 'chat-group-member'></ul>");
     }
-    $("#chat-group-users ul").append(html_result_again);
+    $("#chat-group-users ul").append(htmlResultAgain);
   });
 
-  var keyword_before = "";
+  var keywordBefore = "";
   $("#user_name").keyup(function(e){
       var keyword = $(this).val();
       if(keyword == ""){
-        keyword_before = "";
+        keywordBefore = "";
         $("#chat-group-users ul").remove();
-      }else if(keyword_before != keyword){
-        keyword_before = keyword;
+      }else if(keywordBefore != keyword){
+        keywordBefore = keyword;
         $.ajax({
           type: "GET",
           url: "/users/search.json",
@@ -101,8 +101,8 @@ $(function(){
           dataType: "json"
         })
         .done(function(data){
-          var html_result = buildHTMLNameResultNew(data.users);
-          $("#chat-group-users").append(html_result);
+          var htmlResult = buildHTMLNameResultNew(data.users);
+          $("#chat-group-users").append(htmlResult);
           })
         .fail(function(){
           alert("ajax failed");
