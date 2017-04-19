87360886ce5664e03679314034705ff0299ce387
Climber22 <ihaya.shohei@me.com>
2017-04-19 23:25:55 +0900

    fix json nest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def search
     respond_to do |format|
       format.json {
-        keyword = params[:user_name][:keyword]
+        keyword = params[:keyword]
         @users = User.where("name Like(?)","%#{keyword}%").where.not(id: current_user.id)
       }
     end
