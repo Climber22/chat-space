9ac0e368f31eddefa4fdb682cfd4a3f3d5b958da
Climber22 <ihaya.shohei@me.com>
2017-04-27 21:39:48 +0900

    set validates when only image

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :group
   belongs_to :user
-  validates :body, presence: true
+  validates :body, presence: true, if: "image.nil?"
+  validates :image, presence: true, if: "body.nil?"
 
 end
