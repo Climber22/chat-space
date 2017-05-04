2e69db29e4a1786520b8625f74f87fd40d8dedcb
Climber22 <ihaya.shohei@me.com>
2017-05-05 01:50:00 +0900

    11

--- db/migrate/20170319170806_drop_table_usersgroups.rb
+++ db/migrate/20170319170806_drop_table_usersgroups.rb
@@ -1,8 +1,8 @@
 class DropTableUsersgroups < ActiveRecord::Migration[5.0]
   def change
-    drop_table :users_groups do |t|
-      t.string :name
-      t.timestamps
+    # drop_table :users_groups do |t|
+    #   t.string :name
+    #   t.timestamps
     end
   end
 end
