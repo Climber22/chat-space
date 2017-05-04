e923e2da3df306ba22aeac69c6587bf58dc57811
Climber22 <ihaya.shohei@me.com>
2017-05-05 02:01:01 +0900

    13

--- db/migrate/20170326215607_add_index_to_name_ingroup3.rb
+++ db/migrate/20170326215607_add_index_to_name_ingroup3.rb
@@ -1,6 +1,7 @@
 class AddIndexToNameIngroup3 < ActiveRecord::Migration[5.0]
   def change
     remove_column :groups, :name, :string
-    add_column :groups, :name, :string, null: false
+    add_column :groups, :name, :string
+    change_column :groups, :name, :string, {null: false}
   end
 end
