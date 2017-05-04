85ec4b86ee78fb97da492f655a1a0353ded300b9
Climber22 <ihaya.shohei@me.com>
2017-05-05 02:18:23 +0900

    15

--- db/migrate/20170322133816_adduique_to_name2.rb
+++ db/migrate/20170322133816_adduique_to_name2.rb
@@ -2,6 +2,6 @@ class AdduiqueToName2 < ActiveRecord::Migration[5.0]
   def change
     # remove_column :users, :name, :string
     # add_column :users, :name, :string
-    add_index :users, :name, unique: true
+    # add_index :users, :name, unique: true
   end
 end
