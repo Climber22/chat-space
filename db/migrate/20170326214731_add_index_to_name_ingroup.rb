df4ec7505e1f433382206450ffd83b44e113cf1f
Climber22 <ihaya.shohei@me.com>
2017-05-05 00:37:02 +0900

    9

--- db/migrate/20170326214731_add_index_to_name_ingroup.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToNameIngroup < ActiveRecord::Migration[5.0]
-  def change
-    change_column_null :groups, :name, false
-  end
-end
--- db/migrate/20170326215422_add_index_to_name_ingroup2.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToNameIngroup2 < ActiveRecord::Migration[5.0]
-  def change
-    change_column_null :groups, :name, false
-  end
-end
