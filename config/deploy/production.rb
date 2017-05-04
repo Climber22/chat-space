3b71fba1ad5beaf61da3d6b980545d05ada76ccc
Climber22 <ihaya.shohei@me.com>
2017-05-04 23:46:59 +0900

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '34.209.131.1731', user: 'ec2-user', roles: %w{app db web}
+server '34.209.131.173', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
